647a98ca3bf046d9155120bd8c016c1c7faefc63
Ayush Srivastava <sukunagni@gmail.com>
2023-03-24 14:46:34 +0530

    error callback system and bug fixing

--- endless_MMO_runner/Assets/scripts/lobbymanager.cs
+++ endless_MMO_runner/Assets/scripts/lobbymanager.cs
@@ -19,6 +19,8 @@ public class lobbymanager : MonoBehaviourPunCallbacks
     public Transform content;
     public TMP_Text prefabname;
     public GameObject start_btn;
+    public GameObject error_m;
+   
 
     private void Start()
     {
@@ -46,6 +48,10 @@ public class lobbymanager : MonoBehaviourPunCallbacks
         lobby.SetActive(true);
         room_name.text = "Room Name: " + PhotonNetwork.CurrentRoom.Name;
 
+        if (error_m.active == true)
+        {
+            error_m.SetActive(false);
+        }
        
         foreach (Player p in PhotonNetwork.PlayerList)
         {
@@ -60,13 +66,38 @@ public class lobbymanager : MonoBehaviourPunCallbacks
 
     public void OnClickJoin()
     {
-        PhotonNetwork.JoinRoom(Join_room_name.text);
-        after_connection.SetActive(false);
-        lobby.SetActive(true);
+      
+       PhotonNetwork.JoinRoom(Join_room_name.text);
+        
+        //after_connection.SetActive(false);
+        //lobby.SetActive(true);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        error_m.SetActive(true);
+    }
+
+    public void OnClickLeave_Room()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        if (start_btn.active==true)
+        {
+            start_btn.SetActive(false);
+        }
+        lobby.SetActive(false);
+        after_connection.SetActive(true);
+    }
+
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
     }
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
